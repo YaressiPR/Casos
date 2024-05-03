8e8ba271144eec83dd644ba0ba3efcd988d401f3
YaressiPR <167457168+YaressiPR@users.noreply.github.com>
2024-05-03 17:39:04 -0600

    Add files via upload

---
+++ Encuesta/encabezado.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link href="estilo.css" rel="stylesheet">
+</head>
+<body>
+    <img src="banner_seguridad.png" width="750" height="220"/>
+    <h3 id="centrado">ENCUESTA DE INSEGURIDAD DEL CIUDADANO
+                      EN LA CIUDAD DE LIMA
+    </h3>
+</body>
+</html>
---
+++ Login/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link href="estilo_index.css" rel="stylesheet">
+</head>
+<body>
+    <section>
+        <?php
+        error_reporting(0);
+        session_start();
+
+        if (!isset($_SESSION['admin'])){
+        ?>
+            <form name="frmLogin" method="POST" action="login.php">
+            <table border="3" cellspacing="0" cellpadding="5">
+            <tr>
+                <td colspan="3">
+                    <p id="titulo">Acesso</p>
+                </td>
+            </tr>
+            <tr>
+                <td id="derecha" width="150">Usuario</td>
+                <td><input type="text" name="txtUsuario"
+                           value="" /></td>
+                <td rowspan="4">
+                    <img src="index.png" width="200" height="200" />
+                </td>
+            </tr>
+            <tr>
+                <td id="derecha">Clave</td>
+                <td>
+                    <input type="password" name="txtClave" maxlength="4" />
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="checkbox" name="" value="ON" />
+                           Recordar la clave
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input id="boton" type="submit"
+                           name="btnLogin" value=" LOGIN " />
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                <?php
+                    }else{
+                        header('location:principal.php');
+                    }
+                    if (isset($_SESSION['error'])){
+                        echo $_SESSION['error'];
+                        unset($_SESSION['error']);
+                    }
+                ?>
+                </td>
+            </tr>
+            </table>
+            </form>
+    </section>
+</body>
+</html>
